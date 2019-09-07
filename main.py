7fe9385b2a2c4311504cdb37a24660749041ff21
mantie <mantiereid2@gmail.com>
2019-09-06 23:08:36 -0400

    Added code to download the last 20 events. Now, Just need to add code to anyalze the video using facial recongition.

--- main.py
+++ main.py
@@ -1,15 +1,10 @@
 from ring_doorbell import Ring
+import getpass
+import argparse
 import os
 import config
 
-
-
-
-
-
-
-
-
+eventidlist = []
 myring = Ring(config.username,config.password)
 
 print("is my ring connected?", myring.is_connected)
@@ -38,33 +33,42 @@ for dev in list(myring.stickup_cams + myring.chimes + myring.doorbells):
     dev.volume = 11
     print('Volume:     %s' % dev.volume)
 
+
+
+
+
     for doorbell in myring.doorbells:
 
       # listing the last 15 events of any kind
-      for event in doorbell.history(limit=10000):
+      for event in doorbell.history(limit=20):
         print('ID:       %s' % event['id'])
+
+
         print('Kind:     %s' % event['kind'])
         print('Answered: %s' % event['answered'])
         print('When:     %s' % event['created_at'])
         print('--' * 50)
+        eventidlist.append(event['id'])
+        # print(eventidlist)
 
       # get a event list only the triggered by motion
-      events = doorbell.history(kind='motion')
 
 
     if dev.family == 'chimes':
       dev.test_sound(kind = 'ding')
       dev.test_sound(kind ='motion')
 
-
+print(eventidlist)
 path_for_logs = os.path.join('videos_from_ring', 'videos')
 if not os.path.exists(path_for_logs):  # if folder for the logs does exist, then create it.
   os.makedirs(path_for_logs)
 
 doorbell = myring.doorbells[0]
 
-eventidlist = [6733320337061648197, 6733320654889228101, 6733321161695369029, 6733353472734336837, 6733325576921749317, 6733353472734336837,6733361474258409285]
+
 
 for x in eventidlist:
-  doorbell.recording_download(recording_id=x,filename= str(x) + 'test23.mp4',override=False)
+  doorbell.recording_download(recording_id=x,filename= 'videos_from_ring/videos/' + str(x) + 'test23.mp4',override=False)
+  print(str(x)  + " has been downloaded")
+
 
